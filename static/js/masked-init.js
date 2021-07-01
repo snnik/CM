32d3c4db0f7a44fc46a4ba1101e0e6042f16e6b6
snnik <snnik@live.com>
2021-07-02 00:15:34 +0300

    fix date mask

--- static/js/masked-init.js
+++ static/js/masked-init.js
@@ -4,4 +4,6 @@ $(function() {
         $("#id_passport_number").mask("999 999");
         $("#id_passport_issue_code").mask("999-999");
         $("#id_snils_number").mask("999-999-999 99");
+        $("#id_start_time").mask("99.99.9999 99:99");
+        $("#id_end_time").mask("99.99.9999 99:99");
     });
